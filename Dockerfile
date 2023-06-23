bd90fe4ef6b4d0d1bc7247896ad56eb30a6c3e49
Jonathan Wells <jonathanwells@Jonathans-MacBook-Pro.local>
2023-06-23 08:44:51 +0100

    Github actions added and docker file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:14
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN apt-get update && apt-get install -y cron certbot
+CMD certbot certonly --webroot --agree-tos --email ${CERTBOTEMAIL} -d welljon.xyz -w public --keep-until-expiring --no-eff-email --pre-hook "node main.js" --post-hook "node main-https.js" --test-cert && node main.js
---
+++ public/main-https.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const https = require('https');
+const { Server } = require("socket.io");
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/welljon.xyz/privkey.pem');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/welljon.xyz/fullchain.pem');
+const credentials = {
+  key: privateKey,
+  cert: certificate
+}
+const httpsServer = https.createServer(credentials, app);
+const io = new Server();
+io.attach(httpsServer);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+httpsServer.listen(443, () => {
+  console.log('listening on *:443');
+});
+// Start latency test
+io.on("connection", (socket) => {
+  socket.on("ping", (cb) => {
+    if (typeof cb === "function")
+      cb();
+  });
+});
+// End latency test
